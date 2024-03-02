937850944d420f43d776119c9cd8e49a027a877c
Ponraaj <146065046+Ponraaj@users.noreply.github.com>
2024-03-02 14:09:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 stock_endpoint = "https://www.alphavantage.co/query"
-mail = "ponraajvetrivel@gmail.com"
-password = "osklhfzlmchfvhmb"
-stocks_api_key = "GQ77SGNRR7HJ8UTB"
+mail = #YOUR EMAIL ID
+password = #EMAIL ID PASSWORD
+stocks_api_key = #API_KEY
